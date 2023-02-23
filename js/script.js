37f4d431b2c6e9d189bdc5e568c03c76ea6b7c34
João Pedro C Jardim <joaopedrocjardim@gmail.com>
2023-02-23 13:06:01 -0300

    Ajustes para caso de erro no JS

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const nav = document.querySelectorAll("header nav a")
 
 function adicionalinha(item, index){
     if(window.location.href.includes(item.href)){
-        console.log(item.href)
         nav[index].classList.toggle("ativo")
     }
 }
@@ -56,3 +55,8 @@ function trocarImg(event){
 }
 
 galeria.forEach(eventosGaleria)
+
+//animação 
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
